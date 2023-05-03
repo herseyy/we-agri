39eeaf8fff5a1c413e546fd40f7d07af1dd7d43e
Sloth-iNkt <ciavelm18@gmail.com>
2023-05-03 18:47:22 +0800

    js

---
+++ static/js/plants.js
@@ -0,0 +1,14 @@
+// for every plantItem
+var plantName = document.getElementsByClassName('plantName');
+var plantCat = document.getElementsByClassName('plantCat');
+var plantTemp = document.getElementsByClassName('plantTemp');
+var plantHumid = document.getElementsByClassName('plantHumid');
+var plantRain = document.getElementsByClassName('plantRain');
+var plantGrowth = document.getElementsByClassName('plantGrowth');
+var plantSzn = document.getElementsByClassName('plantSzn');
+
+var plantData = document.getElementsByClassName('plantItem');
+
+$function(){
+    $(plantData).html()
+};
